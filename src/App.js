56678c6a38bd6d2358c24484737e3a00a465cc10
msfartiyal <mayank.fartiyal@affle.com>
2020-02-07 14:26:24 +0530

    added styled components and theming setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+
+import Theme from "./Constants/Theme";
+
+const TR = styled.tr`
+  background-color: ${props => props.theme.dark__grey};
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <ThemeProvider theme={Theme.primary}>
+        Table Map Challenge
+        <table>
+          <thead>
+            <TR>
+              <td>Hi</td>
+              <td>Hi</td>
+              <td>Hi</td>
+              <td>Hi</td>
+            </TR>
+          </thead>
+        </table>
+      </ThemeProvider>
+    </main>
   );
 }
 
---
+++ src/Constants/Theme.js
@@ -0,0 +1,20 @@
+const colors = {
+  dark__green: "#80c464",
+  medium__green: "#a7c941",
+  light__green: "#ebf2d3",
+  sea__green: "#00968a",
+  dark__grey: "#929bac",
+  light__grey: "#f4f5f7",
+  dark__yellow: "#fff2cd",
+  light__yellow: "#fff8e5",
+  black: "#1f1f1f",
+  dark__blue: "#0079d6",
+  light__blue: "#7fbce9"
+};
+
+const theme = {
+  primary: { ...colors },
+  secondary: { ...colors }
+};
+
+export default theme;
