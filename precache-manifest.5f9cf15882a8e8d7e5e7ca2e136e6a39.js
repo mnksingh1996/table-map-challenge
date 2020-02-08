91f807c0ef418f8ed37e42d38159aa990e445f43
Mayank Singh Fartiyal <mnksingh1996@gmail.com>
2020-02-08 19:56:45 +0530

    Updates

---
+++ precache-manifest.5f9cf15882a8e8d7e5e7ca2e136e6a39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b11a3d7ec4b6a65febdaa2ac61a822c",
+    "url": "/table-map-challenge/index.html"
+  },
+  {
+    "revision": "be8b7c2b623b32be3407",
+    "url": "/table-map-challenge/static/js/2.0f01fb54.chunk.js"
+  },
+  {
+    "revision": "fc6b05d7581fe925877ed2c20af4acda",
+    "url": "/table-map-challenge/static/js/2.0f01fb54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f4f35c08932acc7332f",
+    "url": "/table-map-challenge/static/js/main.44c93971.chunk.js"
+  },
+  {
+    "revision": "7ce6c294c1b64da5c862",
+    "url": "/table-map-challenge/static/js/runtime-main.e59fba0e.js"
+  },
+  {
+    "revision": "f73f6161bc332e6808404ce73e862a26",
+    "url": "/table-map-challenge/static/media/sprite.f73f6161.svg"
+  }
+]);
